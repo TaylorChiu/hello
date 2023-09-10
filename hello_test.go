17c57f86c3dc749ce7e6d7fc3337afd718b553c9
TaylorChiu <tai1110kimo@yahoo.com.tw>
2023-09-10 16:57:15 +0800

    hello_test

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,29 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-    got := Hello()
-    want := "Hello, world"
+	/*
+    got := Hello("Taylor")
+    want := "Hello, Taylor"
 
     if got != want {
         t.Errorf("got '%q' want '%q'", got, want)
     }
+	*/
+	t.Run("saying hello to people", func(t *testing.T) {
+        got := Hello("Chris")
+        want := "Hello, Chris"
+
+        if got != want {
+            t.Errorf("got '%q' want '%q'", got, want)
+        }
+    })
+
+    t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
+        got := Hello("")
+        want := "Hello, World"
+
+        if got != want {
+            t.Errorf("got '%q' want '%q'", got, want)
+        }
+    })
 }
